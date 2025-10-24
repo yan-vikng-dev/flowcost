68b0b07fe029c72387aae4bd52f970d7dbf510cd
yan-vikng-dev <yan@vikng.dev>
2025-10-24 21:38:42 +0700

    update defaults

--- packages/data-ops/src/drizzle/helpers.ts
+++ packages/data-ops/src/drizzle/helpers.ts
@@ -1,12 +1,10 @@
-import { sql } from "drizzle-orm"
 import { integer } from "drizzle-orm/sqlite-core"
 
 export const timestamps = {
 	createdAt: integer({ mode: "timestamp_ms" })
-		.default(sql`(unixepoch() * 1000)`)
+		.$default(() => new Date())
 		.notNull(),
 	updatedAt: integer({ mode: "timestamp_ms" })
-		.default(sql`(unixepoch() * 1000)`)
 		.$onUpdate(() => new Date())
 		.notNull(),
 }
